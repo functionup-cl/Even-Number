ce80e4e5c0872cdb5c2fec13fa376acd79a02335
functionup-cl <108424779+functionup-cl@users.noreply.github.com>
2022-07-26 23:43:32 +0530

    Add files via upload

---
+++ W2D2problem 1.js
@@ -0,0 +1,7 @@
+1. //Print all even numbers in first 50 natural numbers
+	 //HINT: google how to determine if a number is even or not
+
+for ( let i=0; i<=50; i=i+2){
+	 if( i%2==0)
+		console.log(i);
+	}
